5eb4dca66b8f7414673dbfd590e171ea9e2116da
@guoyu09403 <6uAqwE_3jSaCokMJcbyw>
2020-09-18 13:00:48 +0800

    Update RcpClient for RpcAutowired proxy service object.

--- netty-rpc-client/src/main/java/com/netty/rpc/client/RpcClient.java
+++ netty-rpc-client/src/main/java/com/netty/rpc/client/RpcClient.java
@@ -1,10 +1,17 @@
 package com.netty.rpc.client;
 
+import com.netty.rpc.annotation.RpcAutowired;
 import com.netty.rpc.client.proxy.RpcService;
 import com.netty.rpc.client.proxy.ObjectProxy;
 import com.netty.rpc.client.connect.ConnectionManager;
 import com.netty.rpc.client.discovery.ServiceDiscovery;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Proxy;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -14,8 +21,9 @@ import java.util.concurrent.TimeUnit;
  * RPC Client（Create RPC proxy）
  *
  * @author luxiaoxun
+ * @author g-yu
  */
-public class RpcClient {
+public class RpcClient implements ApplicationContextAware, InitializingBean, DisposableBean {
     private ServiceDiscovery serviceDiscovery;
     private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(16, 16,
             600L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(1000));
@@ -45,6 +53,37 @@ public class RpcClient {
         threadPoolExecutor.shutdown();
         serviceDiscovery.stop();
         ConnectionManager.getInstance().stop();
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        this.stop();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            Object bean = applicationContext.getBean(beanName);
+            Field[] fields = bean.getClass().getDeclaredFields();
+            try {
+                for (Field field : fields) {
+                    RpcAutowired rpcAutowired = field.getAnnotation(RpcAutowired.class);
+                    if (rpcAutowired != null) {
+                        String version = rpcAutowired.version();
+                        field.setAccessible(true);
+                        field.set(bean, createService(field.getType(), version));
+                    }
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
