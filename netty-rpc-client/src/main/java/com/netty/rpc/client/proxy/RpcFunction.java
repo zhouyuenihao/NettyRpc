9abd211b4c45235f6ff7ee6f460149d993ab1c44
guoyu09403 <guoyu09403@126.com>
2020-09-25 18:50:54 +0800

    In order to support multiple parameters of lambda, is this too complicated? See ServiceTest2 for details. In addition, the getClassType method under ObjectProxy has a BUG, ​​ReferenceType.TYPE would return PrimitiveType, which will cause the reflection of the method to fail.

--- netty-rpc-client/src/main/java/com/netty/rpc/client/proxy/RpcFunction.java
+++ netty-rpc-client/src/main/java/com/netty/rpc/client/proxy/RpcFunction.java
@@ -1,17 +1,17 @@
 package com.netty.rpc.client.proxy;
 
-import java.io.Serializable;
 import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Method;
 
 /**
  * lambda method reference
  * g-yu
+ *
  * @param <T>
  * @param <P>
  */
 @FunctionalInterface
-public interface RpcFunction<T, P> extends Serializable {
+public interface RpcFunction<T, P> extends SerializableFunction<T> {
     /**
      * have parameter
      *
@@ -19,12 +19,7 @@ public interface RpcFunction<T, P> extends Serializable {
      * @param p
      * @return
      */
-    P apply(T t, P p);
+    Object apply(T t, P p);
 
-    default String getName() throws Exception {
-        Method write = this.getClass().getDeclaredMethod("writeReplace");
-        write.setAccessible(true);
-        SerializedLambda serializedLambda = (SerializedLambda) write.invoke(this);
-        return serializedLambda.getImplMethodName();
-    }
+
 }
--- netty-rpc-test/src/main/java/com/app/test/service/HelloService.java
+++ netty-rpc-test/src/main/java/com/app/test/service/HelloService.java
@@ -4,4 +4,6 @@ public interface HelloService {
     String hello(String name);
 
     String hello(Person person);
+
+    String substring(String str, Integer indexOf);
 }
